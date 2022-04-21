29f8bd4d9361e778e8ff371fd544149f30bdf893
roxywasiak <ruksclone@hotmail.com>
2022-04-21 13:35:58 +0100

    was checking to see why my array brings every single character

--- script.js
+++ script.js
@@ -10,10 +10,8 @@ const generatePassword = () => {
   const passwordLength = prompt(
     "Enter a password length of 8 or more and 128 characters or less"
   );
-
   //storing the answer from prompt into passwordLength then getting the string and converting it to a number stored in passwordLengthNum and then returning it.
   const passwordLengthNum = parseInt(passwordLength, 10);
-
   //verifying the condition
   if (passwordLengthNum >= 8 && passwordLengthNum <= 128) {
     //please ask the user to select their criteria eg uppercase/lowercase etc
@@ -24,7 +22,7 @@ const generatePassword = () => {
     const special = confirm("Would you like to include spacial characters?");
     const getCriteria = [];
     if (lowercase) {
-      getCriteria.push([
+      getCriteria.push(
         "a",
         "b",
         "c",
@@ -50,11 +48,11 @@ const generatePassword = () => {
         "w",
         "x",
         "y",
-        "z",
-      ]);
+        "z"
+      );
     }
     if (uppercase) {
-      getCriteria.push([
+      getCriteria.push(
         "A",
         "B",
         "C",
@@ -80,16 +78,16 @@ const generatePassword = () => {
         "W",
         "X",
         "Y",
-        "Z",
-      ]);
+        "Z"
+      );
     }
     if (numbers) {
-      getCriteria.push(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+      getCriteria.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
     }
     if (special) {
-      getCriteria.push([
-        "!”,“#”, “$”, “%”, “&”, “‘”, “(”,“), “*”, “+”, “,”, “-”, “.”, “/”, “:”, “;”, ” < “, “=”, ” > “, ” ? “,“@”,“[”, “\\“, “]”, ” ^ “, “_”,  “`”, “{”,  “|”, “}”  “~",
-      ]);
+      getCriteria.push(
+        "!”,“#”, “$”, “%”, “&”, “‘”, “(”,“), “*”, “+”, “,”, “-”, “.”, “/”, “:”, “;”, ” < “, “=”, ” > “, ” ? “,“@”,“[”, “\\“, “]”, ” ^ “, “_”,  “`”, “{”,  “|”, “}”  “~"
+      );
     }
     return getCriteria;
   } else {
@@ -99,7 +97,7 @@ const generatePassword = () => {
 
 //object array
 const criteria = [
-  {
+  ({
     category: "lowerCase",
     items: [
       "a",
@@ -131,7 +129,6 @@ const criteria = [
     ],
     text: "Would you like to include the category of lowercase?",
   },
-
   {
     category: "upperCase",
     items: [
@@ -164,30 +161,28 @@ const criteria = [
     ],
     text: "Would you like to include the category of uppercase?",
   },
-
   {
     category: "numeric",
     items: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     text: "Would you like to include the category of numeric?",
   },
-
   {
     category: "specialCharacters",
     items: [
       "!”,“#”, “$”, “%”, “&”, “‘”, “(”,“), “*”, “+”, “,”, “-”, “.”, “/”, “:”, “;”, ” < “, “=”, ” > “, ” ? “,“@”,“[”, “\\“, “]”, ” ^ “, “_”,  “`”, “{”,  “|”, “}”  “~",
     ],
     text: "Would you like the category of specialCharacters?",
-  },
+  }),
 ]; // end of my array objects list
 
 //get password criteria function
 
 //get a random password
-const getRandomPassword = (criteria, passwordLengthNum) => {
+const getRandomPassword = (passwordLength, GetCriteria) => {
   const passwordArray = [];
-  for (let i = 0; i < passwordLengthNum; i += 1) {
+  for (let i = 0; i < passwordLength; i += 1) {
     //select random characters from the array
-    const randomCategoryIndex = Math.floor(Math.random() * criteria.length);
+    const randomCategoryIndex = Math.floor(Math.random() * GetCriteria.length);
 
     // get random category
     const randomCategory = criteria[randomCategoryIndex];
